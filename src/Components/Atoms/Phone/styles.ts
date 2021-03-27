095bb8759776960d6484ae68c5116af2aa065fe0
cancio <filipe.cancio@outlook.com>
2021-03-27 17:23:40 -0300

    update colors

--- src/Components/Atoms/Phone/styles.ts
+++ src/Components/Atoms/Phone/styles.ts
@@ -3,10 +3,11 @@ import styled from 'styled-components';
 export const Container = styled.div`
     width: 300px;
     height: 600px;
-    background-color: var(--sm-white-01);
-    color: var(--sm-white-01);
+    background-color: var(--sm-gray-01);
+    color: var(--sm-gray-06);
     border-radius: 20px;
-    border: 10px solid var(--sm-orange-01);
+    border: 10px solid var(--sm-gray-06);
+    overflow:hidden;
     display: flex;
     align-items: center;
     justify-content: center;
--- src/Styles/generic/Colors/index.tsx
+++ src/Styles/generic/Colors/index.tsx
@@ -2,9 +2,28 @@ import { createGlobalStyle } from 'styled-components';
 
 const Colors = createGlobalStyle`
   :root {
-      --sm-gray-01: #717171;
       --sm-white-01: #FFFFFF;
-      --sm-orange-01: #FF8A00;
+
+      --sm-gray-01: #F2F2F2;
+      --sm-gray-02: #D8D8D8;
+      --sm-gray-03: #C8C8C8;
+      --sm-gray-04: #7C7C7C;
+      --sm-gray-05: #717171;
+      --sm-gray-06: #474747;
+
+      --sm-orange-01: #F3832E;
+      --sm-orange-02: #EF7828;
+      --sm-orange-03: #FE511C;
+      --sm-orange-04: #804A28;
+
+      --sm-purple-01: #74309D;
+      --sm-purple-02: #5B2D8C;
+
+      --sm-yellow-01: #FBDE34;
+      
+      --sm-magenta-01: #ED008C;
+
+      --sm-green-01: #219653;
   }
 `;
 
