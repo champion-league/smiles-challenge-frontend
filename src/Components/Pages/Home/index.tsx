87851f15384326fdd27b59ce15c4a49b3719f28e
cancio <filipe.cancio@outlook.com>
2021-03-28 11:09:22 -0300

    #1 Pages Login, Home, SplashScreen

---
+++ src/Components/Pages/Home/index.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import {Container} from './styles';
+
+
+function Index(props:any) {
+
+  return (
+    <Container>
+        Home
+    </Container>
+  );
+}
+
+export default Index;
---
+++ src/Components/Pages/Login/index.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import {Container} from './styles';
+
+
+function Index(props:any) {
+
+  return (
+    <Container>
+        Login
+    </Container>
+  );
+}
+
+export default Index;
