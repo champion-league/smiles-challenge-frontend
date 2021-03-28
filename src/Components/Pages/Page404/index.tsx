fc0ce52bd9aa6c39bf1124aeacc64be1bd624743
cancio <filipe.cancio@outlook.com>
2021-03-28 11:49:43 -0300

    #1 404 page

---
+++ src/Components/Pages/Page404/index.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import {Container} from './styles';
+
+
+function Index(props:any) {
+
+  return (
+    <Container>
+        404
+    </Container>
+  );
+}
+
+export default Index;
--- src/Routes/index.tsx
+++ src/Routes/index.tsx
@@ -6,6 +6,7 @@ import Login from '../Components/Pages/Login'
 import Onboarding from '../Components/Pages/Onboarding'
 import SplashScreen from '../Components/Pages/SplashScreen'
 import TravelDetail from '../Components/Pages/TravelDetail'
+import Page404 from '../Components/Pages/Page404'
 
 const Router = () => (
     <Routes>
@@ -15,6 +16,7 @@ const Router = () => (
         <Route path="/onboarding" component={Onboarding} exact />
         <Route path="/home" component={Home} exact />
         <Route path="/detail" component={TravelDetail} exact />
+        <Route path="/404" component={Page404} exact />
       </Switch>
     </Routes>
   );
