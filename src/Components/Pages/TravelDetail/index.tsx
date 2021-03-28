432834adffc9bce6d28a675eb67c4d69f6c79ce7
cancio <filipe.cancio@outlook.com>
2021-03-28 11:14:10 -0300

    #1 Onboarding e travel detail

---
+++ src/Components/Pages/TravelDetail/index.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import {Container} from './styles';
+
+
+function Index(props:any) {
+
+  return (
+    <Container>
+        TravelDetail
+    </Container>
+  );
+}
+
+export default Index;
